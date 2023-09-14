0f782f86e8c4f96403d72654822fa8db10f7632c
Neal Chandra <chandra.neal@gmail.com>
2023-09-14 01:24:29 -0700

    working on deployed setup

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/docs'
-            : '/api/py/docs',
+            : '/api/docs',
       },
       {
         source: '/api/py/:path*',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/api/py/:path*'
-            : '/api/py',
+            : '/api',
       },
       {
         source: '/openapi.json',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/openapi.json'
-            : '/api/py/openapi.json',
+            : '/api/openapi.json',
       },
     ];
   },
