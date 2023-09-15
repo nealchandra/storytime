0df2c2dfe8410bdbec01343a4beda31f7b8539e9
Neal Chandra <chandra.neal@gmail.com>
2023-09-15 00:33:16 -0700

    fix attempt

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from .utils.prompts import (
+from ._utils.prompts import (
     CHARACTER_BIOS,
     CHARACTER_DESCRIPTORS,
     GENERATE_IMAGE_PROMPT,
@@ -13,7 +13,7 @@ from .utils.prompts import (
     STORY,
     TITLE,
 )
-from .utils.workflow import (
+from ._utils.workflow import (
     GuidanceLLMListStep,
     GuidanceLLMStep,
     ReplicateImageGeneratorStep,
