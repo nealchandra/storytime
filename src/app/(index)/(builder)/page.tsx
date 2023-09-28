8e0d9b577f52ffcfd6d5c3035d13ad503874daac
Neal Chandra <chandra.neal@gmail.com>
2023-09-28 11:59:22 -0700

    Fix builder view on mobile

--- src/app/(index)/(builder)/page.tsx
+++ src/app/(index)/(builder)/page.tsx
@@ -14,7 +14,7 @@ export default async function BuilderPage() {
       <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
         Build your story
       </h1>
-      <div className="hidden h-full flex-col md:flex">
+      <div className="h-full flex-col md:flex">
         <div className="grid gap-12">
           <StoryForm />
         </div>
